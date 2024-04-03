39ba7996110e895f311f05356fe136fb0b5543ec
I-HellCat-I <max_lobov_2005@mail.ru>
2024-04-03 19:00:39 +0300

    В теории, проблемы с потенциальной рекурсией решены.

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ import java.util.Stack;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) {
         Context.getInitDate();
         Interactor interactor = new Interactor();
         String input = "";
